1d42ba5cb91c86159dcaf1fcb8399ee1a54ca6ef
Orhideous <devnull@localhost>
2013-01-27 18:53:29 +0200

    Прочее O_o

---
+++ ponyFiction/stories/templatetags/story_comments_delta.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from django import template
+register = template.Library()
+
+@register.filter
+def story_comments_delta(story, author):
+    return story.last_comments_by_author(author)
