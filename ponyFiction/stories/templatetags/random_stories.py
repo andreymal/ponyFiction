f6de9f17f9928e865b25973b29d058ae888e2f06
Orhideous <devnull@localhost>
2013-02-13 23:47:12 +0200

    #86 fix

--- ponyFiction/stories/templatetags/random_stories.py
+++ ponyFiction/stories/templatetags/random_stories.py
@@ -4,5 +4,5 @@ register = template.Library()
 
 @register.inclusion_tag('random_stories.html')
 def random_stories():
-    random_stories = Story.objects.order_by('?')[0:10]
+    random_stories = Story.published.order_by('?')[0:10]
     return {'random_stories': random_stories}
