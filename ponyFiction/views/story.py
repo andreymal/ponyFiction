eb19e1856b2fb9007b366499b5fd34b70b25672f
Orhideous <devnull@localhost>
2013-05-28 17:28:42 +0300

    important stories accesibility fix!

--- ponyFiction/views/story.py
+++ ponyFiction/views/story.py
@@ -18,7 +18,13 @@ from cacheops.invalidation import invalidate_obj
 
 @csrf_protect
 def story_view(request, pk, comments_page):
-    story = get_object_or_404(Story.objects.accessible(user=request.user), pk=pk)
+    try:
+        story = get_object_or_404(Story.objects.accessible(user=request.user), pk=pk)
+    except Http404:
+        story = get_object_or_404(Story, pk=pk)
+        if not story.editable_by(request.user):
+            raise PermissionDenied
+
     chapters = story.chapter_set.order_by('order')
     comments_list = story.comment_set.order_by('-date').all().cache()
     paged = Paginator(comments_list, settings.COMMENTS_COUNT['page'], orphans=settings.COMMENTS_ORPHANS)
