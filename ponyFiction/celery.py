7357333949c596dc804342046178b9d9c38a9abb
Andriy Kushnir (Orhideous) <orhideous@gmail.com>
2016-02-24 03:01:23 +0200

    Убраны захардкоженные настройки окружения по умолчанию

--- ponyFiction/celery.py
+++ ponyFiction/celery.py
@@ -1,12 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-import os
-
 from celery import Celery
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ponyFiction.settings.development')
-
 from django.conf import settings
 
 app = Celery('ponyFiction')
