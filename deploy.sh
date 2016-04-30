0ee8cf026ad68d3f63d01a09be513547b6e4e79e
Andriy Kushnir (Orhideous) <orhideous@gmail.com>
2016-04-30 03:48:53 +0300

    Добавлен простой скрипт выкатки изменений.

---
+++ deploy.sh
@@ -0,0 +1,98 @@
+#!/usr/bin/env bash
+set -e
+
+SYMLINK_NAME="latest"
+CONTAINERS=(redis memcached sphinx mysql uwsgi-pkg)
+
+function deploy() {
+    cd ${SOURCE_DIR}
+    # Get app version
+    local APP_VERSION=$(git describe --tags)
+    # Cleanup
+    vagga clean
+    # Build app (and static)
+    vagga package
+    # Clean unused containers
+    vagga _clean --unused
+    # Build and sync all containers
+    declare -A VERSIONS
+    for name in ${CONTAINERS[*]}
+    do
+        local full_name=${name}.$(vagga _version_hash --short ${name})
+        local latest_name=${name}.${SYMLINK_NAME}
+
+        if [ -d .vagga/.lnk ]; then
+            root_dir=".vagga/.lnk/.roots/$full_name/root"
+        else
+            root_dir=".vagga/.roots/$full_name/root"
+        fi
+
+        vagga _build ${name}
+
+        rsync --archive \
+            --hard-links \
+            --delete-after \
+            --stats \
+            --delay-updates \
+            --link-dest ${IMAGE_DIR}/${latest_name} \
+            ${root_dir}/ \
+            ${IMAGE_DIR}/${full_name}
+
+        local tmpdir=$(mktemp -d)
+        ln -snf ${full_name} ${tmpdir}/${latest_name}
+        rsync --recursive --links --verbose ${tmpdir}/ ${IMAGE_DIR}/
+        rm -rf ${tmpdir}
+
+        VERSIONS+=([$name]=${full_name})
+    done
+    # Create new config for lithos
+    local NEW_CONFIG=${CONFIG_DIR}/${APP_VERSION}.yaml
+    # Load templating helpers
+    . /usr/local/bin/mo
+    mo ${SOURCE_DIR}/config/prod/lithos/processes/trunk.yaml.template > ${NEW_CONFIG}
+    # Switch all containers to new version
+    lithos_switch trunk ${NEW_CONFIG}
+    cp -f ${NEW_CONFIG} /etc/lithos/processes/trunk.yaml
+}
+
+usage(){
+cat <<'EOT'
+Deploy stories
+
+Пример использования:
+    ./deploy.sh \
+        --config-dir /opt/ponyFiction/config \
+        --image-dir /opt/ponyFiction/image \
+        --source-dir /opt/ponyFiction/source
+
+Параметры:
+   -h, --help           Справка по использованию
+   -c, --config-dir     Путь к конфигурациям дерева процессов lithos
+   -i, --image-dir      Путь к каталогу контейнеров
+   -s, --source-dir     Путь к исходному коду
+EOT
+exit 0;
+}
+
+[ $# -eq 0 ] && usage
+
+while [ $# -gt 0 ]
+do
+    case "$1" in
+        -c|--config-dir) CONFIG_DIR=$2; shift;;
+        -s|--source-dir) SOURCE_DIR=$2; shift;;
+        -i|--image-dir) IMAGE_DIR=$2; shift;;
+        -h|--help) usage;;
+        *) break;;
+    esac
+    shift
+done
+
+if [ -z ${CONFIG_DIR+x} ] ||
+   [ -z ${SOURCE_DIR+x} ] ||
+   [ -z ${IMAGE_DIR+x} ];
+then
+    usage
+else
+    deploy
+fi
