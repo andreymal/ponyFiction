6356f1d91e3dd5d7fd32ebe3c0913ad26d396e5a
Andriy Kushnir (Orhideous) <me@orhideous.name>
2017-02-06 23:56:59 +0200

    Fixed version

--- ponyFiction/templatetags/version.py
+++ ponyFiction/templatetags/version.py
@@ -1,12 +1,9 @@
-from os.path import dirname
-from os.path import join
-
 from django import template
 
 register = template.Library()
 
 try:
-    _version = open(join(dirname(dirname(__file__)), "VERSION")).read().strip()
+    _version = open("backend.version").read().strip()
 except (IOError, OSError) as e:
     _version = "dev"
 
