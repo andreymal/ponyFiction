04ae26df3e42ac7c759498331ffdac5406752c87
Andriy Kushnir (Orhideous) <me@orhideous.name>
2017-02-06 23:56:25 +0200

    Fixed caching

--- ponyFiction/bl/news.py
+++ ponyFiction/bl/news.py
@@ -6,4 +6,4 @@ from .utils import BaseBL
 
 class NewsItemBL(BaseBL):
     def suggest(self):
-        return self.model.objects.filter(visible=True).order_by('-updated').first().cache()
+        return self.model.objects.filter(visible=True).order_by('-updated').cache().first()
