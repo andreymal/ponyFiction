48be4b712fa7d72afe49ecf5b4e96a7f63a9e42f
Andriy Kushnir (Orhideous) <me@orhideous.name>
2017-02-22 00:21:24 +0200

    Refactored templatetags

--- ponyFiction/templatetags/utils.py
+++ ponyFiction/templatetags/utils.py
@@ -1,11 +1,19 @@
+import re
 from django import template
 
 register = template.Library()
+
+RE_NORMALIZE = re.compile(r"\W+")
 
 try:
     _version = open("backend.version").read().strip()
 except (IOError, OSError) as e:
     _version = "dev"
+
+
+@register.filter
+def normspaces(value):
+    return RE_NORMALIZE.sub(" ", value).strip()
 
 
 @register.simple_tag
